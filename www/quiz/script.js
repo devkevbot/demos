db87431ea9eabd0fc647291132a2c2fead45c11d
devkevbot <31908183+devkevbot@users.noreply.github.com>
2024-06-01 19:43:07 -0700

    feat: add more questions to the quiz

--- www/quiz/script.js
+++ www/quiz/script.js
@@ -1,14 +1,36 @@
+/**
+ * HTML Elements
+ */
 const question1 = document.getElementById("question-1");
-const resultField = document.getElementById("result-field");
+const answer1 = document.getElementById("answer-1");
 
-question1.addEventListener("submit", (event) => {
-  const data = new FormData(question1);
-  const answerSubmitted = data.get("question-1");
-  const correctAnswer = "css";
+const question2 = document.getElementById("question-2");
+const answer2 = document.getElementById("answer-2");
 
-  const result = answerSubmitted === correctAnswer ? "correct" : "incorrect";
+const question3 = document.getElementById("question-3");
+const answer3 = document.getElementById("answer-3");
 
-  resultField.innerHTML = `<hr /><p>You were <span class="${result}"><b>${result}!</b></span> The correct answer was: <b>${correctAnswer.toUpperCase()}</b></p>`;
+const questionElems = [question1, question2, question3];
+const answerElem = [answer1, answer2, answer3];
 
-  event.preventDefault();
+/**
+ * Data
+ */
+const correctAnswers = ["CSS", "HyperText Markup Language", "dynamically"];
+
+questionElems.forEach((question, index) => {
+  const position = index + 1;
+
+  question.addEventListener("submit", (event) => {
+    const data = new FormData(question);
+    const userAnswer = data.get(`question-${position}`);
+    const correctAnswer = correctAnswers[index];
+    const result = userAnswer === correctAnswer ? "correct" : "incorrect";
+
+    answerElem[index].innerHTML = `
+<hr /><p>You were <span class="${result}"><b>${result}!</b></span>
+The correct answer was: <b>${correctAnswer}</b></p>`;
+
+    event.preventDefault();
+  });
 });
